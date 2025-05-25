c88ad7b8e8439f6413fd619715e25fee654801f1
Ryan Figg <ryan.figg@gmail.com>
2025-05-25 14:44:10 -0500

    Add 1-20

---
+++ 1-20.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#define TAB 5
+
+/* Write a program "detab" to replace tabs with proper number of spaces
+to reach next tab stop. Fixed set of stops every n columns.
+Should n be a variable or symbolic parameter?
+
+Pseudo:
+read each char, count up columns, count down to next stop. reset count on newline.
+on tab, loop adding spaces until you reach next stop.
+*/
+
+int main()
+{
+  int c, col, nexttab;
+
+  col=0;
+  nexttab = TAB;
+  while ((c = getchar()) != EOF) {
+    // if \n, put char, reset col and nexttab
+    // if \t, loop adding spaces
+    // else put char, count up and down
+    if (c == '\t') {
+      // loop adding spaces
+      while (nexttab > 0) {
+        putchar(' ');
+        ++col;
+        --nexttab;
+      }
+      nexttab = TAB;
+    } else {
+      // put char
+      // if \n, reset col and nexttab
+      // else count up and down, reset nexttab
+      putchar(c);
+      if (c == '\n') {
+        col = 0;
+        nexttab = TAB;
+      } else {
+        ++col;
+        --nexttab;
+        if (nexttab == 0)
+          nexttab = TAB;
+      }
+    }
+  }
+}
