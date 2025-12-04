6f3e3b7e842fb2bbfdd87ec13846a1d4d638aa77
Ryan Figg <ryan.figg@gmail.com>
2025-12-04 00:48:04 -0600

    4-14 corrected.

--- 4-14.c
+++ 4-14.c
@@ -1,5 +1,13 @@
 /* Define macro swap(t,x,y) that interchanges two arguments of type t */
 
-#define swap(t,x,y) t c; c = x; x = y; y = c;
+#define swap(t,x,y) { t _c;   \
+                      _c = x; \
+                      x = y;  \
+                      y = _c; }
 /* Could that work? */
-
+/* Mine would not have worked. The block structure in their answer contains
+their local variable, and they use an underscore variable, assuming that 
+underscore variable will not be used as one of the parameters.
+However the idea and format is there. The block structure avoids creating a new
+variable after the macro. The underscore local variable makes conflict with
+parameter less likely. Slash at end of line makes it format better. */
